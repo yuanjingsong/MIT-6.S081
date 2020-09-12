edfc2e32dd9bb150b11fbd82092cae30e7963cd3
yuanjingsong <532473857@qq.com>
2020-09-12 16:24:21 +0800

    finish sleep function and pass it

---
+++ user/sleep.c
@@ -0,0 +1,14 @@
+#include "kernel/types.h"
+#include "user/user.h"
+#include "kernel/syscall.h"
+
+int main(int argc, char* argv[]) {
+    if (argc <= 1) {
+        fprintf(2, "usage: sleep 10 ...\n");
+        exit(1);
+    }
+    
+    sleep(atoi(argv[1]));
+
+    exit(0);
+}
