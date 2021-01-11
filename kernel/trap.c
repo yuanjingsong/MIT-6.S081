ff75337be05cc7b31472f35e71021b69850b3e98
yuanjingsong <532473857@qq.com>
2021-01-11 15:13:29 +0800

    finish task2

--- kernel/trap.c
+++ kernel/trap.c
@@ -67,6 +67,24 @@ usertrap(void)
     syscall();
   } else if((which_dev = devintr()) != 0){
     // ok
+  } else if (r_scause() == 13 || r_scause() == 15) {
+    // Handle the page fault exception.
+    uint64 va = r_stval();
+    uint64 copy_mem = (uint64)kalloc();
+
+    if (copy_mem == 0) {
+      p -> killed = -1;
+    }else {
+      memset((void*) copy_mem, 0, PGSIZE);
+      va = PGROUNDDOWN(va);
+      if (mappages(p->pagetable, va, PGSIZE, copy_mem, PTE_W|PTE_R|PTE_U) !=0 ) {
+        kfree((void*)copy_mem);
+        p -> killed = -1;
+      }
+    }
+
+
+
   } else {
     printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
     printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
